6e2f406c9b5410cf68cf5d3dc99a6bc6b46242af
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-05-08 19:24:27 +0900

    [Update] インストール時にのみ動的にcontent_scriptを実行

--- background.js
+++ background.js
@@ -1,19 +1,22 @@
 "use strict";
 
-chrome.runtime.onInstalled.addListener(function () {
-	// 読み込み/更新時に既存のタブで実行する
-	chrome.tabs.query({
-		url: "*://*/*"
-	}, function (result) {
-		result.forEach(function (tab) {
-			chrome.tabs.executeScript(tab.id, {
-				file: "wheel2volume.js",
-				allFrames: true
-			}, function (result) {
-				if (typeof result === "undefined") {
-					console.info("ページが読み込まれていません", tab.url, tab);
-				}
+chrome.runtime.onInstalled.addListener(function (details) {
+	// インストール時に既存のタブで実行する
+	// ※ 更新時は実行しない
+	if (details.reason === "install") {
+		chrome.tabs.query({
+			url: "*://*/*"
+		}, function (result) {
+			result.forEach(function (tab) {
+				chrome.tabs.executeScript(tab.id, {
+					file: "wheel2volume.js",
+					allFrames: true
+				}, function (result) {
+					if (typeof result === "undefined") {
+						console.info("ページが読み込まれていません", tab.url, tab);
+					}
+				});
 			});
 		});
-	});
+	}
 });
